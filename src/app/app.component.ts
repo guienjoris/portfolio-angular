4d2f3f6eac9e4be3bfc7d4e2a7eb9ccc228cc7d9
guienjoris <guienjoris@gmail.com>
2020-02-03 11:30:22 +0100

    add close button menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
+import { Router } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 
 
 @Component({
@@ -6,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  
+export class AppComponent implements OnInit{
+  currentComponentStatus: boolean = false;
+  constructor(private _location: Location, private router:Router) 
+  {}
+  backClicked() {
+    this._location.back();
+  }
     ngOnInit(){
+  }
+  onActivate(componentRef:any){
+    console.log(componentRef)
+    if(componentRef === MenuComponent){
+      this.currentComponentStatus = true;
+    }
+    console.log(this.currentComponentStatus)
 
   }
 }
