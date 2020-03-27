7a1b855dfd50e168644c57eec6c2360f951b9ff3
Shinryu <guienjoris@gmail.com>
2020-03-27 17:03:24 +0100

    remove nodemailer for sendmail

--- server.js
+++ server.js
@@ -3,53 +3,27 @@ const app = express()
 var dotenv = require('dotenv').config({path: 'private.env'})
 var cors = require('cors');
 const path = require('path');
+const sendmail = require('sendmail')();
 
 
-var mail = require('nodemailer').mail;
-// var smtpTransport= require('nodemailer-smtp-transport')
-// var transport = mailer.createTransport(smtpTransport({
-//     service: "gmail",
-//     auth:{
-//         user: process.env.MAIL_USER,
-//         pass: process.env.MAIL_MDP
-//     }}));
+
     app.use(express.json());
     app.use(cors());
 
 app.use(express.static(__dirname + '/dist/portfolio-angular'));
 
 app.post('/contact-post',(req,res)=>{
-    let mailContentMe={
+    sendmail({
         from: req.body.mail,
-        to: 'guienjoris@gmail.com',
+        to: `guienjoris@gmail.com, ${req.body.mail}`,
         subject: 'Contact Portfolio',
-        html: `<p>Message:</p> ${req.body.message} 
-        <p>De: </p> ${req.body.name} 
-        <p> Adresse email: </p> ${req.body.mail} `
-    }
-    mail(mailContentMe,(error,req,res)=>{
+        html: `<p>Message: ${req.body.message}</p>  
+        <p>De: ${req.body.name} </p>  
+        <p> Adresse email:${req.body.mail} </p>  `
+    },(error,req,res)=>{
         if(error){
             console.log("Erreur lors de l'envoi de l'email");
             console.log(error)
-            res.status(500).send({err:error})
-        }else{
-            console.log('Email envoyé avec succès!')
-        }
-    })
-    let mailCopy = {
-        from:'guienjoris@gmail.com',
-        to: req.body.mail,
-        subject: ' Copie du mail de contact Portfolio de Guien Joris',
-        html: `<p>Ceci est un mail de copie:" </p>
-        <p>Message:</p> ${req.body.message} 
-        <p>De: </p> ${req.body.name} 
-        <p> Adresse email: </p> ${req.body.mail} <p>"</p>`
-    }
-    mail(mailCopy,(error,req,res)=>{
-        if(error){
-            console.log("Erreur lors de l'envoi de l'email");
-            console.log(error)
-            res.status(500).send({err:error})
         }else{
             console.log('Email envoyé avec succès!')
         }
